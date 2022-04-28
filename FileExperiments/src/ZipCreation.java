3bffd756b8a067b79ffeacaefb7d1e31302f5c3e
Гасан <hasanaliyeev@gmail.com>
2022-04-28 23:53:22 +0300

    added ZipCreation

--- FileExperiments/src/ZipCreation.java
+++ FileExperiments/src/ZipCreation.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -29,5 +30,35 @@ public class ZipCreation {
       e.printStackTrace();
     }
 
+  }
+}
+
+class ZipCreations{
+
+  public static void main(String[] args) {
+    String path = "C:/Users/aliye/Desktop/";
+
+    String[] in = {"orange.jpeg", "file.txt", };
+    String out = "C:/Users/aliye/Desktop/archive.zip";
+
+    try {
+      FileOutputStream outputStream = new FileOutputStream(out);
+      ZipOutputStream zipOut = new ZipOutputStream(outputStream);
+
+      for (String fileName : in) {
+        File file = new File(path + fileName);
+        ZipEntry entry = new ZipEntry(file.getName());
+        zipOut.putNextEntry(entry);
+        Path filePath = Paths.get(file.getAbsolutePath());
+        byte[] data = Files.readAllBytes(filePath);
+        zipOut.write(data);
+      }
+      zipOut.flush();
+      zipOut.close();
+      outputStream.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
